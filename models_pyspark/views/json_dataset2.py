6a8fb6f9ac367d2039b77e5a427da4079f1c30b8
cccs-jc <cccs-jc@cyber.gc.ca>
2022-03-07 16:39:56 +0000

    created datalake simulation

--- models_pyspark/views/json_dataset2.py
+++ models_pyspark/views/json_dataset2.py
@@ -2,6 +2,11 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession._instantiatedSession
 
+
+import os
+full_path = os.path.abspath("./datalake/data.json")
+
 # here we can use Accio to read json files from datalake
-df = spark.read.json("file:///tmp/manifest.json")
+df = spark.read.json("file:///" + full_path)
+
 df.createOrReplaceTempView("pyspark_json_dataset2")
